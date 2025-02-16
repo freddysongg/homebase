baffc1e516addb6b481616817653f47ee40d0a47
Freddy Song <fredsong99@gmail.com>
2025-02-15 23:21:12 -0800

    feat: logging, ci/cd

---
+++ backend/middleware/security.js
@@ -0,0 +1,30 @@
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+const securityMiddleware = (app) => {
+  // Set security HTTP headers
+  app.use(helmet());
+
+  // Rate limiting
+  app.use('/api', limiter);
+
+  // Data sanitization against NoSQL query injection
+  app.use(mongoSanitize());
+
+  // Data sanitization against XSS
+  app.use(xss());
+
+  // Prevent parameter pollution
+  app.use(hpp());
+};
+
+export { securityMiddleware }; 
