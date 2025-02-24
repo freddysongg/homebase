ce8038dfba02b6fbf486b56c7606e8b5951f346b
sunbrandon <sunabrandon@gmail.com>
2025-02-23 21:45:05 -0800

    Create navigation

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Navigation from '@/components/Navigation'
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,11 @@
 import About from '@/components/About';
 import Landing from '@/components/Landing';
-import Footer from '@/components/Footer';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <Landing />
       <About />
-      <Footer />
     </div>
   );
 };
